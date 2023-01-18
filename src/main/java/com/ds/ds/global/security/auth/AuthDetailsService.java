0299b2e0aceaaad1ae52dde9f43fe042423cf511
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-18 21:40:31 +0900

    :sparkles: provider
    dd

--- src/main/java/com/ds/ds/global/security/auth/AuthDetailsService.java
+++ src/main/java/com/ds/ds/global/security/auth/AuthDetailsService.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public class authDetailsService implements UserDetailsService {
+public class AuthDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return null;
