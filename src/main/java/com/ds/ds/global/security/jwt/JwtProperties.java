45dbecca7d4bedff708e29c7767014103e2d5433
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-18 20:56:13 +0900

    :sparkles: properties
    dd

---
+++ src/main/java/com/ds/ds/global/security/jwt/JwtProperties.java
@@ -0,0 +1,15 @@
+package com.ds.ds.global.security.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@ConstructorBinding
+@AllArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private final String accessSecret;
+    private final String refreshSecret;
+}
