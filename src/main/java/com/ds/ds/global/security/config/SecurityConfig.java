6ca5b73ac8dca0f102dfc439c06c05c3789271c6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-18 20:52:29 +0900

    :sparkles: config
    dd

---
+++ src/main/java/com/ds/ds/global/security/config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.ds.ds.global.security.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+}
