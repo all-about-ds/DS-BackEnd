01047cc95fed6eae851b280ea14a27d1369d2c80
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-01-30 23:25:00 +0900

    :sparkles: handler 생성
    및 등록

--- src/main/java/com/ds/ds/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ds/ds/global/error/handler/GlobalExceptionHandler.java
@@ -1,4 +1,19 @@
 package com.ds.ds.global.error.handler;
 
+import com.ds.ds.global.error.ErrorCode;
+import com.ds.ds.global.error.ErrorResponse;
+import com.ds.ds.global.error.exceptions.GlobalException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+    @ExceptionHandler(GlobalException.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(GlobalException e){
+        ErrorCode errorCode =e.getErrorCode();
+        return new ResponseEntity<>(new ErrorResponse(((ErrorCode) errorCode).getStatus(),errorCode.getMessage()),
+                HttpStatus.valueOf(errorCode.getStatus()));
+    }
 }
