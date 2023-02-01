3ace7231c9ce3b258ba10077ad8b185a37be5d71
ssyy0122 <z01039118740@gmail.com>
2023-02-01 16:26:32 +0900

    :sparkles: User Entity design
    User Entity design

---
+++ src/main/java/com/ds/ds/domain/user/domain/entity/User.java
@@ -0,0 +1,31 @@
+package com.ds.ds.domain.user.domain.entity;
+
+import com.ds.ds.global.common.entity.BaseIdEntity;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User extends BaseIdEntity {
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Builder
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
