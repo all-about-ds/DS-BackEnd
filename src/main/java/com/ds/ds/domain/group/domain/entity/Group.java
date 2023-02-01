db4e3f51764d293d35498c862d3769b869aa71a3
ssyy0122 <z01039118740@gmail.com>
2023-02-01 14:35:25 +0900

    :sparkles: BaseEntity 생성
    BaseEntity

---
+++ src/main/java/com/ds/ds/domain/group/domain/entity/Group.java
@@ -0,0 +1,13 @@
+package com.ds.ds.domain.group.domain.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Group {
+
+}
