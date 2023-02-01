36985f3b98ed89208ffd372839fcf9c011525596
ssyy0122 <z01039118740@gmail.com>
2023-02-01 16:32:01 +0900

    :sparkles: mapping
    mapping

--- src/main/java/com/ds/ds/domain/group/domain/entity/Group.java
+++ src/main/java/com/ds/ds/domain/group/domain/entity/Group.java
@@ -1,12 +1,18 @@
 package com.ds.ds.domain.group.domain.entity;
 
+import com.ds.ds.domain.user.domain.entity.User;
 import com.ds.ds.global.common.entity.BaseIdEntity;
 import com.fasterxml.jackson.databind.ser.Serializers;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
@@ -26,5 +32,16 @@ public class Group extends BaseIdEntity {
     @Column
     private String password;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private User user;
+
+    @Builder
+    public Group(String groupDescription, String groupImg, Long groupMaxCount, String password) {
+        this.groupDescription = groupDescription;
+        this.groupImg = groupImg;
+        this.groupMaxCount = groupMaxCount;
+        this.password = password;
+    }
 
 }
