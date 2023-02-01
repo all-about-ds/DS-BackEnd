69888a39317d3c1a324f0d44f67ed10e41e81fd8
ssyy0122 <z01039118740@gmail.com>
2023-02-01 15:13:39 +0900

    :sparkles: entity Column 생성
     entity Column 생성

--- src/main/java/com/ds/ds/domain/group/domain/entity/Group.java
+++ src/main/java/com/ds/ds/domain/group/domain/entity/Group.java
@@ -1,13 +1,30 @@
 package com.ds.ds.domain.group.domain.entity;
 
+import com.ds.ds.global.common.entity.BaseIdEntity;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
 @Getter
 @NoArgsConstructor
-public class Group {
+public class Group extends BaseIdEntity {
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String groupDescription;
+
+    private String groupImg;
+
+    @Column
+    private Long groupMaxCount;
+
+    @Column
+    private String password;
+
 
 }
