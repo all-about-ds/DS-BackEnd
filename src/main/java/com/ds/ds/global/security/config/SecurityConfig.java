eb82171a2271484ede20d2060195edaa24dbd4e3
shgur7236 <shgurtns7236@naver.com>
2023-02-16 20:51:09 +0900

    :sparkles: securityConfig 구현

--- src/main/java/com/ds/ds/global/security/config/SecurityConfig.java
+++ src/main/java/com/ds/ds/global/security/config/SecurityConfig.java
@@ -1,18 +1,59 @@
 package com.ds.ds.global.security.config;
 
+import com.ds.ds.global.security.authentication.CustomAuthenticationEntryPoint;
+import com.ds.ds.global.security.exception.handler.JwtExceptionHandler;
+import com.ds.ds.global.security.filter.JwtFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+    private final JwtExceptionHandler jwtExceptionHandler;
+    private final ObjectMapper objectMapper;
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        return http.build();
+        return http
+                .cors().and()
+                .csrf().disable()
+                .httpBasic().disable()
+                .formLogin().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .mvcMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+
+                //auth
+                .antMatchers(HttpMethod.POST,"/auth/signin").permitAll()
+
+                .anyRequest().denyAll()
+
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new CustomAuthenticationEntryPoint(objectMapper))
+
+                .and()
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterAfter(jwtExceptionHandler, JwtFilter.class)
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
