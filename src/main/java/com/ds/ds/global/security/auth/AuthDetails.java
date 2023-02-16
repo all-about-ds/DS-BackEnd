73c6c865aa8a4537f553990a5174110ba0a17b90
shgur7236 <shgurtns7236@naver.com>
2023-02-16 20:46:18 +0900

    :recycle: AuthDetails 수정

--- src/main/java/com/ds/ds/global/security/auth/AuthDetails.java
+++ src/main/java/com/ds/ds/global/security/auth/AuthDetails.java
@@ -23,26 +23,26 @@ public class AuthDetails implements UserDetails {
 
     @Override
     public String getUsername() {
-        return String.valueOf(user.getIdx());
+        return user.getEmail();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
