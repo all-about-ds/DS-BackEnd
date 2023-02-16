0ef4a43f3b06d64d9b69dd9c736e6e02bacb34fa
shgur7236 <shgurtns7236@naver.com>
2023-02-16 20:45:34 +0900

    :sparkles: AuthController 구현

--- src/main/java/com/ds/ds/domain/auth/presentation/AuthController.java
+++ src/main/java/com/ds/ds/domain/auth/presentation/AuthController.java
@@ -1,4 +1,33 @@
 package com.ds.ds.domain.auth.presentation;
 
+import com.ds.ds.domain.auth.presentation.dto.SignInDto;
+import com.ds.ds.domain.auth.presentation.request.SignInRequestDto;
+import com.ds.ds.domain.auth.presentation.response.TokenResponse;
+import com.ds.ds.domain.auth.service.SignInService;
+import com.ds.ds.domain.auth.util.AuthConverter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+    private final AuthConverter authConverter;
+    private final SignInService signinService;
+
+    /*
+    담당자: 노혁
+    기능: 로그인
+     */
+    @PostMapping("/signin")
+    public ResponseEntity<TokenResponse> signIn(@RequestBody SignInRequestDto signInRequestDto){
+        SignInDto signInDto = authConverter.toDto(signInRequestDto);
+        TokenResponse tokenResponse = signinService.signIn(signInDto);
+        return new ResponseEntity<>(tokenResponse, HttpStatus.OK);
+    }
 }
