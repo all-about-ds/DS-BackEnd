e989f370889fdcbd6230b3dd3d37611bc585d765
shgur7236 <shgurtns7236@naver.com>
2023-02-16 20:50:47 +0900

    :bug: refreshToken 부분 수정

--- src/main/java/com/ds/ds/domain/auth/domain/entity/RefreshToken.java
+++ src/main/java/com/ds/ds/domain/auth/domain/entity/RefreshToken.java
@@ -1,16 +1,20 @@
 package com.ds.ds.domain.auth.domain.entity;
 
+import lombok.*;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
 
+import javax.persistence.Entity;
 import javax.persistence.Id;
-import java.util.UUID;
 
-@RedisHash(value = "refreshToken")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@RedisHash(value = "refreshToken", timeToLive = 7 * 24 * 60 * 60 * 1000)
 public class RefreshToken {
     @Id
     @Indexed
-    private UUID userId = null;
+    private Long userId;
     @Indexed
     private String token;
 }
