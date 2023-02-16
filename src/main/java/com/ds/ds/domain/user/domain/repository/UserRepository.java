cd411087f9ece7a24b9035df254cb2399233f847
shgur7236 <shgurtns7236@naver.com>
2023-02-16 14:48:58 +0900

    :sparkles: UserRepository findUserByEmail 생성

--- src/main/java/com/ds/ds/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/ds/ds/domain/user/domain/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.ds.ds.domain.user.domain.repository;
 import com.ds.ds.domain.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByEmail(String username);
 }
