af644c5a94d723f8c784a3ca09e106843d86d43e
shgur7236 <shgurtns7236@naver.com>
2023-02-16 20:49:06 +0900

    :sparkles: JwtExceptionHandler 비어있는 부분 구현

--- src/main/java/com/ds/ds/global/security/exception/handler/JwtExceptionHandler.java
+++ src/main/java/com/ds/ds/global/security/exception/handler/JwtExceptionHandler.java
@@ -1,5 +1,13 @@
 package com.ds.ds.global.security.exception.handler;
 
+import com.ds.ds.domain.user.exception.UserNotFoundException;
+import com.ds.ds.global.error.ErrorCode;
+import com.ds.ds.global.error.ErrorResponse;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
@@ -8,10 +16,30 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
+@RequiredArgsConstructor
 public class JwtExceptionHandler extends OncePerRequestFilter {
+    private final ObjectMapper objectMapper;
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
         filterChain.doFilter(request,response);
-        //todo Catch로 에러 관리
+        try {
+            filterChain.doFilter(request, response);
+        } catch (ExpiredJwtException e) {
+            setErrorResponse(ErrorCode.EXPIRED_TOKEN, response);
+        } catch (JwtException | IllegalArgumentException e) {
+            setErrorResponse(ErrorCode.INVALID_TOKEN, response);
+        } catch (UserNotFoundException e) {
+            setErrorResponse(ErrorCode.USER_NOT_FOUND, response);
+        }
+    }
+
+    public void setErrorResponse(ErrorCode errorCode, HttpServletResponse response) throws IOException {
+        response.setStatus(errorCode.getStatus());
+        response.setContentType("application/json");
+        ErrorResponse errorResponse = new ErrorResponse(errorCode.getStatus(), errorCode.getMessage());
+        String errorResponseEntityToJson = objectMapper.writeValueAsString(errorResponse);
+        response.getWriter().write(errorResponseEntityToJson);
     }
 }
