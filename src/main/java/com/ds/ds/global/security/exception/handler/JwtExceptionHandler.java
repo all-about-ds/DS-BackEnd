0509ffc2473b8f6954acf3f03aa2342dd042710f
shgur7236 <shgurtns7236@naver.com>
2023-02-17 16:57:57 +0900

    :bug: doFilter 하는게 두번 있어서 오류가 생겼었다.....슬프다...하지만 고쳐서 다행이다

--- src/main/java/com/ds/ds/global/security/exception/handler/JwtExceptionHandler.java
+++ src/main/java/com/ds/ds/global/security/exception/handler/JwtExceptionHandler.java
@@ -23,7 +23,6 @@ public class JwtExceptionHandler extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        filterChain.doFilter(request,response);
         try {
             filterChain.doFilter(request, response);
         } catch (ExpiredJwtException e) {
