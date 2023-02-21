c1373c429e04cc0c073fd4e5739143a88623caad
shgur7236 <shgurtns7236@naver.com>
2023-02-21 16:14:47 +0900

    :sparkles: ADD timeToLive

--- src/main/java/com/ds/ds/domain/auth/domain/entity/RefreshToken.java
+++ src/main/java/com/ds/ds/domain/auth/domain/entity/RefreshToken.java
@@ -7,7 +7,7 @@ import org.springframework.data.redis.core.index.Indexed;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@RedisHash(value = "RefreshToken")
+@RedisHash(value = "RefreshToken", timeToLive = 60L * 60 * 24 * 7)
 public class RefreshToken{
     @Id
     @Indexed
