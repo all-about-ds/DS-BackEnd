337b1f12616964359e87e6717c14ecd58a56071f
shgur7236 <shgurtns7236@naver.com>
2023-03-02 23:45:27 +0900

    :sparkles: AuthCode Repository 생성

---
+++ src/main/java/com/ds/ds/domain/auth/domain/repository/AuthCodeRepository.java
@@ -0,0 +1,7 @@
+package com.ds.ds.domain.auth.domain.repository;
+
+import com.ds.ds.domain.auth.domain.entity.AuthCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthCodeRepository extends JpaRepository<AuthCode, String> {
+}
