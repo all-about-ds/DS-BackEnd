e10ceb2921767d987b37dc1d8b3af1acabf279ba
ssyy0122 <z01039118740@gmail.com>
2023-03-02 19:52:35 +0900

    :sparkles: 텍스트버퍼와 바이너리버 사이즈 설정
    ad

--- src/main/java/com/ds/ds/global/webSocket/config/WebSocketConfig.java
+++ src/main/java/com/ds/ds/global/webSocket/config/WebSocketConfig.java
@@ -1,4 +1,34 @@
 package com.ds.ds.global.webSocket.config;
+import io.netty.channel.ChannelHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
-public class WebSocketConfig {
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSocket
+
+public class WebSocketConfig implements WebSocketConfigurer {
+    private final ChannelHandler channelHandler;
+
+    private static final int MESSAGE_BUFFER_SIZE = 8192;
+
+    @Bean
+    public ServletServerContainerFactoryBean createServletServerContainerFactoryBean() {
+        ServletServerContainerFactoryBean container = new ServletServerContainerFactoryBean();
+        container.setMaxTextMessageBufferSize(MESSAGE_BUFFER_SIZE);
+        container.setMaxBinaryMessageBufferSize(MESSAGE_BUFFER_SIZE);
+        return container;
+    }
+
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+
+    }
 }
