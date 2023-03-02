c2c43061ac42b5feb0466db867f1086681284168
shgur7236 <shgurtns7236@naver.com>
2023-03-02 16:26:05 +0900

    :fire: 주석 제거

--- src/main/java/com/ds/ds/domain/auth/presentation/data/request/SignInRequest.java
+++ src/main/java/com/ds/ds/domain/auth/presentation/data/request/SignInRequest.java
@@ -7,10 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @Builder
 @RequiredArgsConstructor
-public class SignInRequest { // 정규식은 협의 헌 후 다시 할 예정
-//    @Pattern(regexp = "^(?:\\w+\\.?)*\\w+@(?:\\w+\\.)+\\w+$", message = "이메일 형식이 올바르지 않습니다.")
+public class SignInRequest {
     private final String email;
-
-//    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private final String password;
 }
