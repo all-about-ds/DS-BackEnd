62af90ebcdd645bc4541e94c99974581e050161b
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-02 15:17:54 +0900

    :recycle: 네이밍 수정

--- src/main/java/com/ds/ds/domain/group/presentation/data/response/GroupInfoResponse.java
+++ src/main/java/com/ds/ds/domain/group/presentation/data/response/GroupInfoResponse.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @Builder
 @RequiredArgsConstructor
-public class GroupInfoResponseDto {
+public class GroupInfoResponse {
     private final String groupName;
     private final String groupImg;
     private final String groupDescription;
