fb456564a50e68a73e7e3148f479bab6f4116597
ssyy0122 <z01039118740@gmail.com>
2023-03-02 20:00:51 +0900

    :fire: import remove
    dd

--- src/main/java/com/ds/ds/global/webSocket/config/WebSocketConfig.java
+++ src/main/java/com/ds/ds/global/webSocket/config/WebSocketConfig.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
-import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
