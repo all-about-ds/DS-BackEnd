3e5fba0ad9f5b75705a1ed07db79a16f0dabec30
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-02 15:45:16 +0900

    :recycle: requestDto 생성

--- src/main/java/com/ds/ds/domain/group/presentation/data/request/GroupListRequest.java
+++ src/main/java/com/ds/ds/domain/group/presentation/data/request/GroupListRequest.java
@@ -1,4 +1,16 @@
 package com.ds.ds.domain.group.presentation.data.request;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
 public class GroupListRequest {
+    private final Integer Size;
+    private final Integer page;
+    private final Optional<String> keyword;
 }
