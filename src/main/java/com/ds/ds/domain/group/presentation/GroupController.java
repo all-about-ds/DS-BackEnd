a40446d1e7b0404148721be61cd222534afcaff4
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-02 14:59:21 +0900

    :sparkles: group 패키징과 컨트롤러 생성

---
+++ src/main/java/com/ds/ds/domain/group/presentation/GroupController.java
@@ -0,0 +1,11 @@
+package com.ds.ds.domain.group.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/gruop")
+public class GroupController {
+}
