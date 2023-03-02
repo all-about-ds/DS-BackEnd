f8d3db77c66d5b105b5eab0fe6618abf060507c0
ssyy0122 <z01039118740@gmail.com>
2023-03-02 20:00:07 +0900

    :sparkles: WebSocket Cofig
    add

--- src/main/java/com/ds/ds/global/webSocket/config/WebSocketConfig.java
+++ src/main/java/com/ds/ds/global/webSocket/config/WebSocketConfig.java
@@ -3,16 +3,17 @@ import io.netty.channel.ChannelHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSocket
-
 public class WebSocketConfig implements WebSocketConfigurer {
     private final ChannelHandler channelHandler;
 
@@ -29,6 +30,10 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-
+        registry.addHandler((WebSocketHandler) channelHandler,"/channels")
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
+                .setAllowedOrigins("*")
+                .withSockJS()
+                .setSuppressCors(true);
     }
 }
