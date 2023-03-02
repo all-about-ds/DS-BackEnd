8e648920c8bcfa0dfdfb9f4da109dcc4c12847f5
shgur7236 <shgurtns7236@naver.com>
2023-03-02 15:58:02 +0900

    :sparkles: Dto 생성

---
+++ src/main/java/com/ds/ds/domain/auth/presentation/data/dto/SignUpDto.java
@@ -0,0 +1,14 @@
+package com.ds.ds.domain.auth.presentation.data.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class SignUpDto {
+    private final String name;
+    private final String email;
+    private final String password;
+}
