60e31c16452e167f9efaef272941ce7ef9cb1419
shgur7236 <shgurtns7236@naver.com>
2023-03-03 10:01:57 +0900

    :sparkles: Converter 구현

--- src/main/java/com/ds/ds/domain/auth/util/AuthConverter.java
+++ src/main/java/com/ds/ds/domain/auth/util/AuthConverter.java
@@ -2,11 +2,11 @@ package com.ds.ds.domain.auth.util;
 
 import com.ds.ds.domain.auth.domain.entity.AuthCode;
 import com.ds.ds.domain.auth.domain.entity.RefreshToken;
-import com.ds.ds.domain.auth.presentation.data.dto.SignInDto;
-import com.ds.ds.domain.auth.presentation.data.dto.SignUpDto;
-import com.ds.ds.domain.auth.presentation.data.dto.TokenDto;
+import com.ds.ds.domain.auth.presentation.data.dto.*;
 import com.ds.ds.domain.auth.presentation.data.request.SignInRequest;
 import com.ds.ds.domain.auth.presentation.data.request.SignupRequest;
+import com.ds.ds.domain.auth.presentation.data.response.CheckAuthCodeResponse;
+import com.ds.ds.domain.auth.presentation.data.response.SendAuthCodeResponse;
 import com.ds.ds.domain.auth.presentation.data.response.TokenResponse;
 import com.ds.ds.domain.user.domain.entity.User;
 
@@ -18,4 +18,6 @@ public interface AuthConverter {
     RefreshToken toEntity(User user, String refreshToken);
     User toEntity(SignUpDto signUpDto);
     AuthCode toEntity(String email, String code);
+    CheckAuthCodeDto toDto(String code);
+    CheckAuthCodeResponse toResponse(CheckAuthCodeDto checkAuthCodeDto);
 }
