04d06842be506650ab038f120f8a715d114abc50
shgur7236 <shgurtns7236@naver.com>
2023-03-03 10:00:58 +0900

    :sparkles: 메서드 생성

--- src/main/java/com/ds/ds/domain/auth/domain/repository/AuthCodeRepository.java
+++ src/main/java/com/ds/ds/domain/auth/domain/repository/AuthCodeRepository.java
@@ -4,4 +4,5 @@ import com.ds.ds.domain.auth.domain.entity.AuthCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AuthCodeRepository extends JpaRepository<AuthCode, String> {
+    AuthCode findByCode(String authCode);
 }
