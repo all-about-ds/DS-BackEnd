92ce0f12aec0b61c5de42db1b86f84f6441f38c6
shgur7236 <shgurtns7236@naver.com>
2023-03-06 11:15:03 +0900

    :truck: JWT Exception auth로 이동

--- src/main/java/com/ds/ds/domain/auth/exception/UnsupportedJwtTokenException.java
+++ src/main/java/com/ds/ds/domain/auth/exception/UnsupportedJwtTokenException.java
@@ -1,4 +1,4 @@
-package com.ds.ds.global.security.exception;
+package com.ds.ds.domain.auth.exception;
 
 import com.ds.ds.global.error.ErrorCode;
 import com.ds.ds.global.error.exceptions.GlobalException;
--- src/main/java/com/ds/ds/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/ds/ds/global/error/handler/GlobalExceptionHandler.java
@@ -1,15 +1,8 @@
 package com.ds.ds.global.error.handler;
 
-import com.ds.ds.domain.auth.exception.DuplicateEmailException;
-import com.ds.ds.domain.auth.exception.DuplicateNameException;
-import com.ds.ds.domain.auth.exception.ExpiredTokenException;
-import com.ds.ds.domain.auth.exception.InValidAuthCodeException;
-import com.ds.ds.domain.user.exception.PasswordNotMatchException;
-import com.ds.ds.domain.user.exception.UserNotFoundException;
 import com.ds.ds.global.error.ErrorCode;
 import com.ds.ds.global.error.ErrorResponse;
 import com.ds.ds.global.error.exceptions.GlobalException;
-import com.ds.ds.global.security.exception.InvalidJwtSignatureException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
