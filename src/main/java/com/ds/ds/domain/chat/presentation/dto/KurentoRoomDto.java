ca531df2c219e9551b39e8f94d7598d88dcdd083
ssyy0122 <z01039118740@gmail.com>
2023-03-06 18:25:12 +0900

    :sparkles: 룸정보 add
    add

---
+++ src/main/java/com/ds/ds/domain/chat/presentation/dto/KurentoRoomDto.java
@@ -0,0 +1,52 @@
+package com.ds.ds.domain.chat.presentation.dto;
+
+import com.sun.istack.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.kurento.client.KurentoClient;
+import org.kurento.client.MediaPipeline;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Closeable;
+import java.util.concurrent.ConcurrentMap;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class KurentoRoomDto extends ChatRoomDto implements Closeable {
+    //로깅 객체생성
+    private final Logger logger = LoggerFactory.getLogger(KurentoRoomDto.class);
+
+    private KurentoClient kurentoClient;
+
+    private MediaPipeline pipeline;
+
+    @NotNull
+    private String roomId;
+    private String roomName;
+    private int userCount;
+    private int maxUserCnt;
+
+    private String roomPwd;
+    private boolean secretChk;
+    private ChatType chatType;
+
+    private ConcurrentMap<String, KurentoUserSession> participants;
+
+    //룸 정보
+    public void setRoomInfo(String roomId, String roomName, boolean secure, int userCount, int maxUserCnt, ChatType chatType, KurentoClient kurento){
+        this.roomId = roomId;
+        this.roomName = roomName;
+        this.roomPwd = roomPwd;
+        this.secretChk = secure;
+        this.userCount = userCount;
+        this.maxUserCnt = maxUserCnt;
+        this.chatType = chatType;
+        this.kurento = kurento;
+        this.participants = (ConcurrentMap<String, KurentoUserSession>) this.userList;
+}
+
--- src/main/java/com/ds/ds/domain/chat/service/RtcChatService.java
+++ src/main/java/com/ds/ds/domain/chat/service/RtcChatService.java
@@ -1,11 +1,25 @@
 package com.ds.ds.domain.chat.service;
 
+import com.ds.ds.domain.chat.presentation.dto.ChatRoomDto;
+import com.ds.ds.domain.chat.presentation.dto.KurentoRoomDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.kurento.client.KurentoClient;
 import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class RtcChatService {
+    private final KurentoClient kurento;
+
+    public KurentoRoomDto createChatRoom(String roomName, String roomPwd, boolean secretChk, int maxUserCht){
+
+        KurentoRoomDto room = new KurentoRoomDto();
+        String roomId = UUID.randomUUID().toString();
+        room.setRoomInfo(roomId, roomName, secretChk, 0, maxUserCht, ChatRoomDto.ChatType.RTC, kurento);
+
+    }
 }
