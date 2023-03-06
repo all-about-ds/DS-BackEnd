573dfb40dd1c0f9e9131cddfaf648183867dba12
shgur7236 <shgurtns7236@naver.com>
2023-03-06 20:28:16 +0900

    :sparkles: Service 구현

--- src/main/java/com/ds/ds/domain/auth/service/Impl/SearchPasswordServiceImpl.java
+++ src/main/java/com/ds/ds/domain/auth/service/Impl/SearchPasswordServiceImpl.java
@@ -1,20 +1,28 @@
 package com.ds.ds.domain.auth.service.Impl;
 
-import com.ds.ds.domain.auth.presentation.data.dto.PasswordDto;
 import com.ds.ds.domain.auth.presentation.data.dto.SearchPasswordDto;
 import com.ds.ds.domain.auth.service.SearchPasswordService;
 import com.ds.ds.domain.user.domain.entity.User;
-import com.ds.ds.domain.user.domain.repository.UserRepository;
+import com.ds.ds.domain.user.exception.PasswordNotMatchException;
+import com.ds.ds.domain.user.util.UserUtil;
+import com.ds.ds.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class SearchPasswordServiceImpl implements SearchPasswordService {
-    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserUtil userUtil;
     @Override
-    public PasswordDto search(SearchPasswordDto searchPasswordDto) {
+    public void search(SearchPasswordDto searchPasswordDto) {
         if(!searchPasswordDto.getPassword().equals(searchPasswordDto.getCheckPassword())){
+            throw new PasswordNotMatchException(ErrorCode.PASSWORD_NOT_MATCH);
         }
+
+        String encodingPassword = passwordEncoder.encode(searchPasswordDto.getPassword());
+        User user = userUtil.findUserByEmail(searchPasswordDto.getEmail());
+        user.updatePassword(encodingPassword);
     }
 }
