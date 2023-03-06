f0cc0f42eeb95c797c3b82de35606be5c7e7fba4
shgur7236 <shgurtns7236@naver.com>
2023-03-06 17:27:19 +0900

    :sparkles: request 생성

---
+++ src/main/java/com/ds/ds/domain/auth/presentation/data/request/SearchPasswordRequest.java
@@ -0,0 +1,11 @@
+package com.ds.ds.domain.auth.presentation.data.request;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class SearchPasswordRequest {
+}
