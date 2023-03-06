fa844cecc0db10047573c80ae96757b054b8b44f
ssyy0122 <z01039118740@gmail.com>
2023-03-06 09:23:28 +0900

    :sparkles: Room을 담기위한 클래스 생성(싱글톤으로생성(
    add

--- src/main/java/com/ds/ds/domain/chat/presentation/dto/ChatRoomMap.java
+++ src/main/java/com/ds/ds/domain/chat/presentation/dto/ChatRoomMap.java
@@ -1,4 +1,20 @@
 package com.ds.ds.domain.chat.presentation.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+@Getter
+@Setter
 public class ChatRoomMap {
+    private static ChatRoomMap chatRoomMap = new ChatRoomMap();
+    private ConcurrentMap<String, ChatRoomDto> chatRooms = new ConcurrentHashMap<>();
+
+    private ChatRoomMap(){}
+
+    public static ChatRoomMap getInstance(){
+        return chatRoomMap;
+    }
 }
