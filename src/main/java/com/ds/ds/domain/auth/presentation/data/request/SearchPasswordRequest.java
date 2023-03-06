34b18c3c871e27e151af6291f3a90e84b1ed93ce
shgur7236 <shgurtns7236@naver.com>
2023-03-06 20:28:00 +0900

    :recycle: dto 수정

--- src/main/java/com/ds/ds/domain/auth/presentation/data/request/SearchPasswordRequest.java
+++ src/main/java/com/ds/ds/domain/auth/presentation/data/request/SearchPasswordRequest.java
@@ -1,12 +1,12 @@
 package com.ds.ds.domain.auth.presentation.data.request;
 
-import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
 public class SearchPasswordRequest {
+    private final String email;
     private final String password;
     private final String checkPassword;
 }
