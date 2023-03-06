fde03915bfcc2a487dab14c3da4005774ecf0356
shgur7236 <shgurtns7236@naver.com>
2023-03-06 18:13:46 +0900

    :fire: @EnableWebMvc 제거

--- src/main/java/com/ds/ds/global/web/WebConfig.java
+++ src/main/java/com/ds/ds/global/web/WebConfig.java
@@ -2,11 +2,9 @@ package com.ds.ds.global.web;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override //CORS 설정
