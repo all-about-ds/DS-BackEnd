fa62fa753de742e02a2705d3a0945cf28cb938a0
shgur7236 <shgurtns7236@naver.com>
2023-03-06 20:27:28 +0900

    :recycle: 패스워드 -> password

--- src/main/java/com/ds/ds/global/error/ErrorCode.java
+++ src/main/java/com/ds/ds/global/error/ErrorCode.java
@@ -8,7 +8,7 @@ import lombok.Getter;
 @AllArgsConstructor
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
-    PASSWORD_NOT_MATCH(400, "패스워드가 일치하지 않습니다."),
+    PASSWORD_NOT_MATCH(400, "Password 가 일치하지 않습니다."),
     EXPIRED_TOKEN(401, "만료된 토큰 입니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     UNAUTHORIZED(401, "권한이 없습니다."),
