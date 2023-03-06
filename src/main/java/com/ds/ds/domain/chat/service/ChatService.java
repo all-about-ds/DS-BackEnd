05cef475cb3ce35d06560d6f4f87fb50fe262431
ssyy0122 <z01039118740@gmail.com>
2023-03-06 16:50:05 +0900

    :sparkles: ChatService add
    add

---
+++ src/main/java/com/ds/ds/domain/chat/service/ChatService.java
@@ -0,0 +1,15 @@
+package com.ds.ds.domain.chat.service;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Slf4j
+public class ChatService {
+}
