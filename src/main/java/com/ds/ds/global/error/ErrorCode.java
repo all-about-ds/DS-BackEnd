47708d289db666a878ec3181f743ef0971f14dfb
shgur7236 <shgurtns7236@naver.com>
2023-03-06 20:43:33 +0900

    :recycle: 404 -> 400

--- src/main/java/com/ds/ds/global/error/ErrorCode.java
+++ src/main/java/com/ds/ds/global/error/ErrorCode.java
@@ -9,6 +9,7 @@ import lombok.Getter;
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
 public enum ErrorCode {
     PASSWORD_NOT_MATCH(400, "Password 가 일치하지 않습니다."),
+    INVALID_AUTH_CODE(400, "올바르지 않은 인증코드입니다."),
     EXPIRED_TOKEN(401, "만료된 토큰 입니다."),
     INVALID_TOKEN(401, "유효하지 않은 토큰입니다."),
     UNAUTHORIZED(401, "권한이 없습니다."),
@@ -16,7 +17,6 @@ public enum ErrorCode {
     UNSUPPORTED_JWT(401, "지원되지 않는 JWT 토큰입니다."),
     ILLEGAL_ARGUMENT_JWT(401, "JWT 토큰이 잘못되었습니다."),
     USER_NOT_FOUND(404, "유저를 찾을 수 없습니다."),
-    INVALID_AUTH_CODE(404, "올바르지 않은 인증코드입니다."),
     DUPLICATE_NAME(409, "이미 존재하는 닉네임입니다."),
     DUPLICATE_EMAIL(409, "이미 존재하는 이메일입니다."),
     INTERNAL_SERVER_ERROR(500, "internal server error");
