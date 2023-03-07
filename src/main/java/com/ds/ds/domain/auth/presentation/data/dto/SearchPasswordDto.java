13f234cba445bb62eb1e5eb0454ee4fdd42065fb
shgur7236 <shgurtns7236@naver.com>
2023-03-07 09:27:38 +0900

    :recycle: ADD @Builder

--- src/main/java/com/ds/ds/domain/auth/presentation/data/dto/SearchPasswordDto.java
+++ src/main/java/com/ds/ds/domain/auth/presentation/data/dto/SearchPasswordDto.java
@@ -1,9 +1,11 @@
 package com.ds.ds.domain.auth.presentation.data.dto;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
+@Builder
 @RequiredArgsConstructor
 public class SearchPasswordDto {
     private final String email;
