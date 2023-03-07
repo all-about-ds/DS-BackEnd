87f6e37a52bef9494cfa6f264c2eaf848b68fe9b
ssyy0122 <z01039118740@gmail.com>
2023-03-07 23:12:36 +0900

    :sparkles: endpoint 설정
    add

---
+++ src/main/java/com/ds/ds/domain/chatting/config/SpringConfig.java
@@ -0,0 +1,18 @@
+package com.ds.ds.domain.chatting.config;
+
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class SpringConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws-stomp") // 연결된 엔드포인트
+                .withSockJS();
+    }
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        //메세지를 구독하는 요청 url -> 매세지 받을 때
+        registry.enableSimpleBroker("sub");
+    }
+}
