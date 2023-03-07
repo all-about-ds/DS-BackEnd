50788ec655f2bbddbd10d3af83cf824742f78830
shgur7236 <shgurtns7236@naver.com>
2023-03-07 17:18:14 +0900

    :truck: config 파일 생성

--- src/main/java/com/ds/ds/global/redis/config/RedisConfig.java
+++ src/main/java/com/ds/ds/global/redis/config/RedisConfig.java
@@ -1,11 +1,10 @@
-package com.ds.ds.global.redis;
+package com.ds.ds.global.redis.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
