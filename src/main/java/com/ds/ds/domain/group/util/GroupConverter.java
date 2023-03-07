2a4b7c3bdaff29805f4817620969b8a4641a5109
shgur7236 <shgurtns7236@naver.com>
2023-03-07 12:01:34 +0900

    :recycle: clean

--- src/main/java/com/ds/ds/domain/group/util/GroupConverter.java
+++ src/main/java/com/ds/ds/domain/group/util/GroupConverter.java
@@ -4,7 +4,6 @@ import com.ds.ds.domain.group.domain.entity.Group;
 import com.ds.ds.domain.group.presentation.data.dto.GroupDto;
 import com.ds.ds.domain.group.presentation.data.dto.GroupListDto;
 import com.ds.ds.domain.group.presentation.data.dto.GroupListSearchRequirementDto;
-import com.ds.ds.domain.group.presentation.data.request.GroupListRequest;
 import com.ds.ds.domain.group.presentation.data.response.GroupListResponse;
 import com.ds.ds.domain.group.presentation.data.response.GroupResponse;
 import org.springframework.data.domain.Pageable;
@@ -16,7 +15,6 @@ public interface GroupConverter {
     GroupListSearchRequirementDto toDto(Pageable pageable, Optional<String> keyword);
     GroupDto toDto(Group group, Long memberCount);
     GroupListDto toDto(Pageable pageable, List<GroupDto> dto);
-
     GroupResponse toResponse(GroupDto dto);
     GroupListResponse toResponse(Pageable pageable, List<GroupResponse> response);
 }
