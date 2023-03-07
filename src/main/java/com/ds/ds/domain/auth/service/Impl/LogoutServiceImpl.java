2d2088406a5511cb35816012c946f7794056aa81
shgur7236 <shgurtns7236@naver.com>
2023-03-07 17:19:37 +0900

    :sparkles: LogoutService 구현

---
+++ src/main/java/com/ds/ds/domain/auth/service/Impl/LogoutServiceImpl.java
@@ -0,0 +1,30 @@
+package com.ds.ds.domain.auth.service.Impl;
+
+import com.ds.ds.domain.auth.domain.repository.RefreshTokenRepository;
+import com.ds.ds.domain.auth.service.LogoutService;
+import com.ds.ds.domain.user.domain.entity.User;
+import com.ds.ds.domain.user.util.UserUtil;
+import com.ds.ds.global.redis.util.RedisUtil;
+import com.ds.ds.global.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LogoutServiceImpl implements LogoutService {
+    private final JwtProvider jwtProvider;
+    private final RedisUtil redisUtil;
+    private final UserUtil userUtil;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    @Override
+    public void logout(String accessToken) {
+        jwtProvider.validateToken(accessToken, JwtProvider.TokenType.ACCESS_TOKEN);
+
+        User user = userUtil.currentUser();
+        refreshTokenRepository.deleteById(user.getIdx());
+
+        Long expiration = jwtProvider.getExpiration(accessToken);
+        redisUtil.setBlackList(accessToken, "access_token", expiration);
+    }
+}
