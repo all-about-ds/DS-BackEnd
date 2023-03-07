300af95060549ba6d1006b3615c30ad6091ce4bb
ssyy0122 <z01039118740@gmail.com>
2023-03-07 23:19:05 +0900

    :sparkles: 엔드포인트 추가
    add

--- src/main/java/com/ds/ds/domain/chatting/config/SpringConfig.java
+++ src/main/java/com/ds/ds/domain/chatting/config/SpringConfig.java
@@ -13,6 +13,9 @@ public class SpringConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         //메세지를 구독하는 요청 url -> 매세지 받을 때
-        registry.enableSimpleBroker("sub");
+         registry.enableSimpleBroker("sub");
+
+         //메세지를 발생하는 요청 url -> 매세지 보낼때
+        registry.setApplicationDestinationPrefixes("/pub");
     }
 }
