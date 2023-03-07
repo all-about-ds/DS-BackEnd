dd22c2b55ebef6fa84af6fe3b2251ec9e8e0d929
shgur7236 <shgurtns7236@naver.com>
2023-03-07 20:35:49 +0900

    :sparkles: 이미 존재하는 블랙리스트인지 체크

--- src/main/java/com/ds/ds/global/security/filter/JwtFilter.java
+++ src/main/java/com/ds/ds/global/security/filter/JwtFilter.java
@@ -1,7 +1,10 @@
 package com.ds.ds.global.security.filter;
 
+import com.ds.ds.domain.auth.exception.BlackListAlreadyExistException;
+import com.ds.ds.global.error.ErrorCode;
 import com.ds.ds.global.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
@@ -17,6 +20,7 @@ import java.io.IOException;
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtProvider jwtProvider;
+    private final RedisTemplate redisTemplate;
 
     @Override
     protected void doFilterInternal(
@@ -27,6 +31,8 @@ public class JwtFilter extends OncePerRequestFilter {
         if(token != null ){
             Authentication authentication = jwtProvider.getAuthentication(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
+            if(redisTemplate.opsForValue().get(token) != null)
+                throw new BlackListAlreadyExistException(ErrorCode.BLACKLIST_ALREADY_EXIST);
         }
         filterChain.doFilter(request,response);
     }
