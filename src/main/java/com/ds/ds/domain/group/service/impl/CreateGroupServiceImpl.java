1acdf1fd881c272e4bd74224021da96b1b5d3125
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-08 08:52:04 +0900

    :sparkles: 서비스 생성

---
+++ src/main/java/com/ds/ds/domain/group/service/impl/CreateGroupServiceImpl.java
@@ -0,0 +1,13 @@
+package com.ds.ds.domain.group.service.impl;
+
+import com.ds.ds.domain.group.presentation.data.dto.CreateGroupDto;
+import com.ds.ds.domain.group.service.CreateClubService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateGroupServiceImpl implements CreateClubService {
+    @Override
+    public void createClub(CreateGroupDto dto) {
+        
+    }
+}
