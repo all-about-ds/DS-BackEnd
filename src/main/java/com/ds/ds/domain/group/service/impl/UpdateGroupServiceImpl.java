332e16f3e4ea59dbaaf37d14746f2ab1149a8843
shgur7236 <shgurtns7236@naver.com>
2023-03-08 11:17:22 +0900

    :recycle: 로직 수정

--- src/main/java/com/ds/ds/domain/group/service/impl/UpdateGroupServiceImpl.java
+++ src/main/java/com/ds/ds/domain/group/service/impl/UpdateGroupServiceImpl.java
@@ -1,7 +1,9 @@
 package com.ds.ds.domain.group.service.impl;
 
 import com.ds.ds.domain.group.domain.entity.Group;
+import com.ds.ds.domain.group.domain.entity.GroupSecret;
 import com.ds.ds.domain.group.domain.repository.GroupRepository;
+import com.ds.ds.domain.group.domain.repository.GroupSecretRepository;
 import com.ds.ds.domain.group.exception.GroupNotFoundException;
 import com.ds.ds.domain.group.presentation.data.dto.UpdateGroupDto;
 import com.ds.ds.domain.group.service.UpdateGroupService;
@@ -15,15 +17,17 @@ import javax.transaction.Transactional;
 @RequiredArgsConstructor
 public class UpdateGroupServiceImpl implements UpdateGroupService {
     private final GroupRepository groupRepository;
+    private final GroupSecretRepository groupSecretRepository;
     @Override
     @Transactional
     public void updateGroup(Long groupIdx, UpdateGroupDto updateGroupDto) {
         Group group = groupRepository.findById(groupIdx)
                 .orElseThrow(() -> new GroupNotFoundException(ErrorCode.GROUP_NOT_FOUND));
-
         group.updateGroup(updateGroupDto);
-        if(updateGroupDto.getSecret()){
 
+        GroupSecret groupSecret = groupSecretRepository.findByGroupIdx(group.getIdx());
+        if(updateGroupDto.getSecret()){
+            groupSecret.updatePassword(updateGroupDto.getPassword().toString());
         }
     }
 }
