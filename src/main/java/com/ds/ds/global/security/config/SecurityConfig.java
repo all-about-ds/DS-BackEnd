4625245e0805e98b184e4ee33d7d592129952c65
shgur7236 <shgurtns7236@naver.com>
2023-03-08 11:14:51 +0900

    :sparkles: SecurityConfig

--- src/main/java/com/ds/ds/global/security/config/SecurityConfig.java
+++ src/main/java/com/ds/ds/global/security/config/SecurityConfig.java
@@ -49,6 +49,7 @@ public class SecurityConfig {
                 //group
                 .antMatchers(HttpMethod.GET, "/group").permitAll()
                 .antMatchers(HttpMethod.GET, "/group/detail/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/group").authenticated()
                 .antMatchers(HttpMethod.PATCH, "/group/**").authenticated()
 
                 .anyRequest().denyAll()
