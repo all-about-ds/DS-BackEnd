4f37ebe99bc895ac614a7535a335d8d83ec5459b
ssyy0122 <z01039118740@gmail.com>
2023-03-08 12:01:06 +0900

    :fire: code실수
    add

--- src/main/java/com/ds/ds/domain/chatting/domain/repository/ChatRepository.java
+++ src/main/java/com/ds/ds/domain/chatting/domain/repository/ChatRepository.java
@@ -8,17 +8,4 @@ import java.util.*;
 
 @Repository
 public class ChatRepository {
-    private Map<String, ChatRoom> chatRoomMap;
-
-    @PostConstruct
-    private void init() {
-        chatRoomMap = new LinkedHashMap<>();
-    }
-    //전체 채팅방 조회
-    public List<ChatRoom> findAllRoom() {
-        List<ChatRoom> chatRooms = new ArrayList<>(chatRoomMap.values());
-        Collections.reverse(chatRooms);
-
-        return chatRooms;
-    }
 }
