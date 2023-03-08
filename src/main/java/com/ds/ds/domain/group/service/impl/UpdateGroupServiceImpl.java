c883a0da89f1272f1f07d0431d55af19dac03ec5
shgur7236 <shgurtns7236@naver.com>
2023-03-08 12:21:22 +0900

    :recycle: 방장만 할수있게 수정

--- src/main/java/com/ds/ds/domain/group/service/impl/UpdateGroupServiceImpl.java
+++ src/main/java/com/ds/ds/domain/group/service/impl/UpdateGroupServiceImpl.java
@@ -5,8 +5,11 @@ import com.ds.ds.domain.group.domain.entity.GroupSecret;
 import com.ds.ds.domain.group.domain.repository.GroupRepository;
 import com.ds.ds.domain.group.domain.repository.GroupSecretRepository;
 import com.ds.ds.domain.group.exception.GroupNotFoundException;
+import com.ds.ds.domain.group.exception.NotBossException;
 import com.ds.ds.domain.group.presentation.data.dto.UpdateGroupDto;
 import com.ds.ds.domain.group.service.UpdateGroupService;
+import com.ds.ds.domain.user.domain.entity.User;
+import com.ds.ds.domain.user.util.UserUtil;
 import com.ds.ds.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,11 +21,18 @@ import javax.transaction.Transactional;
 public class UpdateGroupServiceImpl implements UpdateGroupService {
     private final GroupRepository groupRepository;
     private final GroupSecretRepository groupSecretRepository;
+    private final UserUtil userUtil;
     @Override
     @Transactional
     public void updateGroup(Long groupIdx, UpdateGroupDto updateGroupDto) {
+        User user = userUtil.currentUser();
         Group group = groupRepository.findById(groupIdx)
                 .orElseThrow(() -> new GroupNotFoundException(ErrorCode.GROUP_NOT_FOUND));
+
+        if(!group.getUser().getIdx().equals(user.getIdx())){
+            throw new NotBossException(ErrorCode.NOT_BOSS);
+        }
+
         group.updateGroup(updateGroupDto);
 
         GroupSecret groupSecret = groupSecretRepository.findByGroupIdx(group.getIdx());
