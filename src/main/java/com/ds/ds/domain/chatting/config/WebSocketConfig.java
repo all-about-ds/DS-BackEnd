4fa946d712aff4a28fa0cdc5a07093ad3e1039cd
ssyy0122 <z01039118740@gmail.com>
2023-03-09 22:45:16 +0900

    :fire: BrokerConfigure 삭제
    add

--- src/main/java/com/ds/ds/domain/chatting/config/WebSocketConfig.java
+++ src/main/java/com/ds/ds/domain/chatting/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/stomp/chat")
-                .setAllowedOrigins("htpp://localhost:8080")
+                .setAllowedOrigins("htpp://localhost:3306")
                 .withSockJS();
     }
     @Override
