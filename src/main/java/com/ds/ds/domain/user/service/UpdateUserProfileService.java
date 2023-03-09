dd96d111cb18dc18e6cf8820196a2b45ecba9917
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-09 17:26:58 +0900

    Revert ":sparkles: 서비스 구현"
    This reverts commit 00e98d76b59425f7138a080673aba3dae8ebbbd6.

--- src/main/java/com/ds/ds/domain/user/service/UpdateUserProfileService.java
+++
@@ -1,7 +0,0 @@
-package com.ds.ds.domain.user.service;
-
-import com.ds.ds.domain.user.presentation.data.dto.UpdateUserProfileDto;
-
-public interface UpdateUserProfileService {
-    void updateUserProfile(UpdateUserProfileDto dto);
-}
