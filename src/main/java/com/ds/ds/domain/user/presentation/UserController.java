9316292ad3924ef66cdf4373875e8ae2fde59765
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-09 17:59:07 +0900

    :sparkles: 컨트롤러 구현

--- src/main/java/com/ds/ds/domain/user/presentation/UserController.java
+++ src/main/java/com/ds/ds/domain/user/presentation/UserController.java
@@ -1,15 +1,15 @@
 package com.ds.ds.domain.user.presentation;
 
 import com.ds.ds.domain.user.presentation.data.dto.UserDto;
+import com.ds.ds.domain.user.presentation.data.request.UpdateUserProfileImgRequest;
 import com.ds.ds.domain.user.presentation.data.response.UserResponse;
 import com.ds.ds.domain.user.service.FindUserService;
+import com.ds.ds.domain.user.service.UpdateUserProfileService;
 import com.ds.ds.domain.user.util.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -20,6 +20,7 @@ import java.util.stream.Collectors;
 public class UserController {
     private final FindUserService findUserService;
     private final UserConverter userConverter;
+    private final UpdateUserProfileService updateUserProfileService;
 
     @GetMapping
     public ResponseEntity<UserResponse> findUser() {
@@ -31,5 +32,12 @@ public class UserController {
         UserResponse result = userConverter.toResponse(dto, groupResponse);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> updateUserProfile(@RequestBody UpdateUserProfileImgRequest request) {
+        updateUserProfileService.updateUserProfileImg(userConverter.toDto(request));
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
