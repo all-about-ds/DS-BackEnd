9ad745f2c5d1936d97d4d633d6d77a7d6573f3e6
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-09 17:26:51 +0900

    Revert ":sparkles: 컨트롤러 구현"
    This reverts commit c0f86991e9aa9a1931c4d87b981ae75dfecafcaf.

--- src/main/java/com/ds/ds/domain/user/presentation/UserController.java
+++ src/main/java/com/ds/ds/domain/user/presentation/UserController.java
@@ -1,16 +1,17 @@
 package com.ds.ds.domain.user.presentation;
 
 import com.ds.ds.domain.user.presentation.data.dto.UserDto;
-import com.ds.ds.domain.user.presentation.data.request.UpdateUserProfileRequest;
 import com.ds.ds.domain.user.presentation.data.response.UserResponse;
 import com.ds.ds.domain.user.service.FindUserService;
-import com.ds.ds.domain.user.service.UpdateUserProfileService;
 import com.ds.ds.domain.user.service.WithdrawUserService;
 import com.ds.ds.domain.user.util.UserConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,7 +23,6 @@ public class UserController {
     private final FindUserService findUserService;
     private final UserConverter userConverter;
     private final WithdrawUserService withdrawUserService;
-    private final UpdateUserProfileService updateUserProfileService;
 
     @GetMapping
     public ResponseEntity<UserResponse> findUser() {
@@ -34,13 +34,6 @@ public class UserController {
         UserResponse result = userConverter.toResponse(dto, groupResponse);
 
         return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    @PatchMapping
-    public ResponseEntity<Void> updateUserProfileImg(@RequestBody UpdateUserProfileRequest request) {
-        updateUserProfileService.updateUserProfile(userConverter.toDto(request));
-
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
     @DeleteMapping
