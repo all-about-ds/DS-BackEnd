c6fc49b9665872c9669d6203960dd14d9a20a18b
shgur7236 <shgurtns7236@naver.com>
2023-03-09 20:58:29 +0900

    :sparkles: dto 생성

---
+++ src/main/java/com/ds/ds/domain/group/presentation/data/dto/JoinGroupDto.java
@@ -0,0 +1,14 @@
+package com.ds.ds.domain.group.presentation.data.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Optional;
+
+@Getter
+@Builder
+@RequiredArgsConstructor
+public class JoinGroupDto {
+    private final Optional<String> password;
+}
