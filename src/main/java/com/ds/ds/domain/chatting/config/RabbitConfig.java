7d815f37eb2e32794dce6264a7b489b5cdd57a43
ssyy0122 <z01039118740@gmail.com>
2023-03-11 18:36:52 +0900

    :sparkles: queue 등록
    add

--- src/main/java/com/ds/ds/domain/chatting/config/RabbitConfig.java
+++ src/main/java/com/ds/ds/domain/chatting/config/RabbitConfig.java
@@ -1,4 +1,16 @@
 package com.ds.ds.domain.chatting.config;
 
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRabbit
 public class RabbitConfig {
+    private static final String CHAT_QUEUE_NAME = "chat.queue";
+    private static final String CHAT_EXCHANGE_NAME = "chat.exchange";
+    private static final String ROUTING_KEY = "room.*";
 }
+@Bean
+public Queue quese() {return new Queue(CHAT_QUEUE_NAME, true);}
