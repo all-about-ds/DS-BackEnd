d46196c6065f4d8dff3c4dfd7afead15b178844b
ssyy0122 <z01039118740@gmail.com>
2023-03-11 18:49:30 +0900

    :sparkles: Queue와 Exchange 바인딩
    add

--- src/main/java/com/ds/ds/domain/chatting/config/RabbitConfig.java
+++ src/main/java/com/ds/ds/domain/chatting/config/RabbitConfig.java
@@ -1,5 +1,8 @@
 package com.ds.ds.domain.chatting.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.context.annotation.Bean;
@@ -13,4 +16,15 @@ public class RabbitConfig {
     private static final String ROUTING_KEY = "room.*";
 }
 @Bean
-public Queue quese() {return new Queue(CHAT_QUEUE_NAME, true);}
+public Queue queue() {return new Queue(CHAT_QUEUE_NAME, true);}
+
+@Bean
+public TopicExchange exchange(){ return new TopicExchange(CHAT_EXCHANGE_NAME); }
+
+
+@Bean
+public Binding binding(Queue queue, TopicExchange exchange) {
+    return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
+}
+
+
