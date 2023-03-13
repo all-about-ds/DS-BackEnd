1806e10fff3bac5e85bc48b147dec980b85f78c7
ssyy0122 <z01039118740@gmail.com>
2023-03-13 11:18:12 +0900

    :fire: import 삭제
    add

--- src/main/java/com/ds/ds/domain/chatting/presentation/StompRabbitController.java
+++ src/main/java/com/ds/ds/domain/chatting/presentation/StompRabbitController.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
