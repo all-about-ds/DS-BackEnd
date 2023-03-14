5049e70bb7bbe135f98a9e14513e12901934a12b
ssyy0122 <z01039118740@gmail.com>
2023-03-14 11:45:39 +0900

    :heavy_plus_sign: 의존성 추가
    add

---
+++ src/main/java/com/ds/ds/domain/chatting/config/redis/RedisConfig.java
@@ -0,0 +1,21 @@
+package com.ds.ds.domain.chatting.config.redis;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+
+@RequiredArgsConstructor
+@Configuration
+public class RedisConfig {
+    /*
+     * redis pub/sub 메시지를 처리하는 listener 설정
+     */
+    @Bean
+    public RedisMessageListenerContainer redisMessageListener(RedisConnectionFactory connectionFactory) {
+        RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        return  container;
+    }
+}
