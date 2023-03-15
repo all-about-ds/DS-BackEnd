1fbb189a0617db50be091b2ef3fac8528df34d26
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-16 07:46:27 +0900

    :sparkles: 컨트롤러 구현

--- src/main/java/com/ds/ds/domain/timer/presentation/TimerController.java
+++ src/main/java/com/ds/ds/domain/timer/presentation/TimerController.java
@@ -1,6 +1,10 @@
 package com.ds.ds.domain.timer.presentation;
 
+import com.ds.ds.domain.timer.service.StartTimerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/timer")
 public class TimerController {
+    private final StartTimerService startTimerService;
+
+    @PatchMapping("/start")
+    public ResponseEntity<Void> startTimer() {
+        startTimerService.startTimer();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
 
