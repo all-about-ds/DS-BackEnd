bf09184e5d2d0ee675e03f8a57a7577d611ea740
ssyy0122 <z01039118740@gmail.com>
2023-03-15 23:14:25 +0900

    :sparkles: ChatController 생성

---
+++ src/main/java/com/ds/ds/domain/chatting/presentation/ChatController.java
@@ -0,0 +1,53 @@
+package com.ds.ds.domain.chatting.presentation;
+
+import com.ds.ds.domain.chatting.domain.entity.ChatMessage;
+import com.ds.ds.domain.chatting.presentation.data.dto.ChatRoom;
+import com.ds.ds.domain.chatting.service.ChatService;
+import com.ds.ds.domain.user.util.Impl.UserUtilImpl;
+import com.ds.ds.global.security.jwt.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+    private final ChatService chatService;
+    private final RedisTemplate redisTemplate;
+    private final ChannelTopic channelTopic;
+
+    private final UserUtilImpl userUtil;
+
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message, @Header("token") String token) {
+        String nickname = userUtil.currentUser().getName();
+        // 로그인 회원 정보로 대화명 설정
+        message.setSender(nickname);
+        // 채팅방 입장시에는 대화명과 메시지를 자동으로 세팅한다.
+        if (ChatMessage.MessageType.ENTER.equals(message.getType())) {
+            message.setSender("[알림]");
+            message.setMessage(nickname + "님이 입장하셨습니다.");
+        }
+        // Websocket에 발행된 메시지를 redis로 발행(publish)
+        redisTemplate.convertAndSend(channelTopic.getTopic(), message);
+    }
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+
+
+}
