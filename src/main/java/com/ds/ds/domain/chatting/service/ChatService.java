78685fb55553d6a782af1e9c344d19a3302bc908
ssyy0122 <z01039118740@gmail.com>
2023-03-15 23:02:46 +0900

    조회,모두조회,채팅룸 만들기

--- src/main/java/com/ds/ds/domain/chatting/service/ChatService.java
+++ src/main/java/com/ds/ds/domain/chatting/service/ChatService.java
@@ -2,13 +2,22 @@ package com.ds.ds.domain.chatting.service;
 
 import com.ds.ds.domain.chatting.presentation.data.dto.ChatRoom;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import groovy.util.logging.Slf4j;
+import groovy.util.logging.Log4j;
+import groovyjarjarantlr.collections.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
+
+
 
 @Slf4j
 @RequiredArgsConstructor
@@ -21,4 +30,25 @@ public class ChatService {
     private void init() {
         chatRooms = new LinkedHashMap<>();
     }
+    public List<ChatRoom> findAllRoom() {
+        return new ArrayList<>(chatRooms.values());
+    }
+    public ChatRoom findRoomById(String roomId) {
+        return chatRooms.get(roomId);
+    }
+
+    public ChatRoom createRoom(String name) {
+        String randomId = UUID.randomUUID().toString();
+        ChatRoom chatRoom = new ChatRoom(randomId, name);
+
+        chatRooms.put(randomId, chatRoom);
+        return chatRoom;
+    }
+
+    public <T> void sendMessage(WebSocketSession session, T message) {
+        try {
+            session.sendMessage(new TextMessage(objectMapper.writeValueAsString(message)));
+        } catch (IOException e) {
+            log.error(e.getMessage(), e);
+        }
 }
