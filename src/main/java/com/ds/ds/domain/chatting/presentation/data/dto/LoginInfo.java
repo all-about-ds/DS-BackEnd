c343ef0545e813507a2bc8d26c85d110e2f57e86
ssyy0122 <z01039118740@gmail.com>
2023-03-16 10:24:31 +0900

    :sparkles: LoginInfo 값에 token 추가

--- src/main/java/com/ds/ds/domain/chatting/presentation/data/dto/LoginInfo.java
+++ src/main/java/com/ds/ds/domain/chatting/presentation/data/dto/LoginInfo.java
@@ -6,10 +6,11 @@ import lombok.Getter;
 @Getter
 public class LoginInfo {
     private final String name;
+    private final String token;
 
     @Builder
-    public LoginInfo(String name) {
+    public LoginInfo(String name, String token) {
         this.name = name;
+        this.token = token;
     }
 }
-
