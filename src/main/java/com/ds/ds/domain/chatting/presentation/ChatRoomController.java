51cd04fc67aa66b5f3ca77b605d7567d0a2b7224
ssyy0122 <z01039118740@gmail.com>
2023-03-16 16:15:34 +0900

    :sparkles: ChatRoom Controller add
    add

--- src/main/java/com/ds/ds/domain/chatting/presentation/ChatRoomController.java
+++ src/main/java/com/ds/ds/domain/chatting/presentation/ChatRoomController.java
@@ -1,6 +1,7 @@
 package com.ds.ds.domain.chatting.presentation;
 
 import com.ds.ds.domain.chatting.domain.repository.ChatRoomRepository;
+import com.ds.ds.domain.chatting.presentation.data.dto.ChatRoom;
 import com.ds.ds.domain.chatting.presentation.data.dto.LoginInfo;
 import com.ds.ds.domain.user.util.Impl.UserUtilImpl;
 import com.ds.ds.global.security.jwt.JwtProvider;
@@ -8,9 +9,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Controller
@@ -25,7 +27,40 @@ public class ChatRoomController {
     public LoginInfo getUserInfo() {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         String name = auth.getName();
-        return LoginInfo.builder().name(name).token(jwtProvider.ge).build();
+        return LoginInfo.builder().name(name).token(jwtProvider.generateToken(name)).build();
+    }
+    // 채팅 리스트 화면
+    @GetMapping("/room")
+    public String rooms(Model model) {
+        return "/chat/room";
+    }
+
+    // 모든 채팅방 목록 반환
+    @GetMapping("/rooms")
+    @ResponseBody
+    public List<ChatRoom> room() {
+        return chatRoomRepository.findAllRoom();
+    }
+
+    // 채팅방 생성
+    @PostMapping("/room")
+    @ResponseBody
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatRoomRepository.createChatRoom(name);
+    }
+
+    // 채팅방 입장 화면
+    @GetMapping("/room/enter/{roomId}")
+    public String roomDetail(Model model, @PathVariable String roomId) {
+        model.addAttribute("roomId", roomId);
+        return "/chat/roomdetail";
+    }
+
+    // 특정 채팅방 조회
+    @GetMapping("/room/{roomId}")
+    @ResponseBody
+    public ChatRoom roomInfo(@PathVariable String roomId) {
+        return chatRoomRepository.findRoomById(roomId);
     }
 
 }
