673594ef0bf6d239284acaab4e7495173645b647
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-16 10:26:23 +0900

    :sparkles: 컨트롤러 구현

--- src/main/java/com/ds/ds/domain/timer/presentation/TimerController.java
+++ src/main/java/com/ds/ds/domain/timer/presentation/TimerController.java
@@ -1,10 +1,14 @@
 package com.ds.ds.domain.timer.presentation;
 
+import com.ds.ds.domain.timer.presentation.data.request.StopTimerRequest;
 import com.ds.ds.domain.timer.service.StartTimerService;
+import com.ds.ds.domain.timer.service.StopTimerService;
+import com.ds.ds.domain.timer.util.TimerConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/timer")
 public class TimerController {
     private final StartTimerService startTimerService;
+    private final StopTimerService stopTimerService;
+    private final TimerConverter timerConverter;
 
     @PatchMapping("/start")
     public ResponseEntity<Void> startTimer() {
         startTimerService.startTimer();
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PatchMapping("/stop")
+    public ResponseEntity<Void> stopTimer(@RequestBody StopTimerRequest request) {
+        stopTimerService.stopTimer(timerConverter.toDto(request));
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
