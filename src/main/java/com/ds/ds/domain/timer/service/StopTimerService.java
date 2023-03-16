20e66210e3985d6ce594a4e25071ca57f61dca7f
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-16 10:20:23 +0900

    :sparkles: 서비스 구현

---
+++ src/main/java/com/ds/ds/domain/timer/service/StopTimerService.java
@@ -0,0 +1,7 @@
+package com.ds.ds.domain.timer.service;
+
+import com.ds.ds.domain.timer.presentation.data.dto.StopTimerDto;
+
+public interface StopTimerService {
+    void stopTimer(StopTimerDto dto);
+}
