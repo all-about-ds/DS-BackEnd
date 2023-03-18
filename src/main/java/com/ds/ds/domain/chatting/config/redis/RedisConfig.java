a97dc51433077c2ee45729ed8bf3c734601b6575
ssyy0122 <z01039118740@gmail.com>
2023-03-18 19:29:30 +0900

    :bug: 실수한 import 고침
    add

--- src/main/java/com/ds/ds/domain/chatting/config/redis/RedisConfig.java
+++ src/main/java/com/ds/ds/domain/chatting/config/redis/RedisConfig.java
@@ -1,11 +1,14 @@
 package com.ds.ds.domain.chatting.config.redis;
 
+import com.ds.ds.domain.chatting.service.RedisSubscriber;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
