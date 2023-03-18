3f7e423cf3684fc2c82997497813fc82aae367d6
ssyy0122 <z01039118740@gmail.com>
2023-03-18 19:45:28 +0900

    :sparkles: interceptors 생성
    add

--- src/main/java/com/ds/ds/domain/chatting/config/WebSocketConfig.java
+++ src/main/java/com/ds/ds/domain/chatting/config/WebSocketConfig.java
@@ -1,7 +1,9 @@
 package com.ds.ds.domain.chatting.config;
 
+import com.ds.ds.domain.chatting.handler.StompHandler;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -11,6 +13,7 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final StompHandler stompHandler;
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
         config.enableSimpleBroker("/sub");
@@ -21,6 +24,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chatting").setAllowedOrigins("*")
                 .withSockJS();
+    }
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(stompHandler);
     }
 
 }
