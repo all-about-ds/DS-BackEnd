6c1a3bf553852135fc239090150c0bf2efbf956c
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-18 22:06:23 +0900

    :sparkles: config 생성

---
+++ src/main/java/com/ds/ds/domain/group/config/WebSocketConfig.java
@@ -0,0 +1,16 @@
+package com.ds.ds.domain.group.config;
+
+import com.ds.ds.domain.group.handler.GroupWebSocketHandler;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(new GroupWebSocketHandler(), "/group");
+    }
+}
---
+++ src/main/java/com/ds/ds/domain/group/handler/GroupWebSocketHandler.java
@@ -0,0 +1,8 @@
+package com.ds.ds.domain.group.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class GroupWebSocketHandler extends TextWebSocketHandler {
+}
