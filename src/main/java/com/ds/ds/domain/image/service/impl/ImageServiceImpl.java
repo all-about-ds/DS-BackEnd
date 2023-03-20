a9173965a024b7c862d314a7a8feb333217ed8fb
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-20 19:59:30 +0900

    :sparkles: 서비스 구현

--- src/main/java/com/ds/ds/domain/image/service/impl/ImageServiceImpl.java
+++ src/main/java/com/ds/ds/domain/image/service/impl/ImageServiceImpl.java
@@ -1,16 +1,49 @@
 package com.ds.ds.domain.image.service.impl;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.ds.ds.domain.image.presentaion.data.dto.ImageDto;
 import com.ds.ds.domain.image.service.ImageService;
+import com.ds.ds.domain.image.util.ImageConverter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Value("${cloud.aws.s3.url}")
+    private String url;
+
+    private final AmazonS3 amazonS3;
+    private final ImageConverter imageConverter;
+
     @Override
     public ImageDto uploadImage(MultipartFile file) {
-        return null;
+        String fileName = createFileName(file.getOriginalFilename());
+        ObjectMetadata objectMetadata = new ObjectMetadata();
+        objectMetadata.setContentLength(file.getSize());
+        objectMetadata.setContentType(file.getContentType());
+        try (InputStream inputStream = file.getInputStream()) {
+            amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                    .withCannedAcl(CannedAccessControlList.PublicRead));
+            return imageConverter.toDto(url + fileName);
+        } catch (IOException e) {
+            throw new RuntimeException("파일 업로드에 실패했습니다.");
+        }
+    }
+
+    private String createFileName(String fileName) {
+        return UUID.randomUUID().toString();
     }
 }
