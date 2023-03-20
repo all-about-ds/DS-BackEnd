55acfc54f5366e119b61b3a7eab4897b97737da0
윤지빈 <80810278+yoonjibin@users.noreply.github.com>
2023-03-20 19:54:41 +0900

    :sparkles: config 구현

--- src/main/java/com/ds/ds/domain/image/service/ImageService.java
+++ src/main/java/com/ds/ds/domain/image/service/ImageService.java
@@ -1,5 +1,8 @@
 package com.ds.ds.domain.image.service;
 
-public interface ImageService {
+import com.ds.ds.domain.image.presentaion.data.dto.ImageDto;
+import org.springframework.web.multipart.MultipartFile;
 
+public interface ImageService {
+    ImageDto uploadImage(MultipartFile file);
 }
--- src/main/java/com/ds/ds/domain/image/service/impl/ImageServiceImpl.java
+++ src/main/java/com/ds/ds/domain/image/service/impl/ImageServiceImpl.java
@@ -1,10 +1,16 @@
 package com.ds.ds.domain.image.service.impl;
 
+import com.ds.ds.domain.image.presentaion.data.dto.ImageDto;
 import com.ds.ds.domain.image.service.ImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor
 public class ImageServiceImpl implements ImageService {
+    @Override
+    public ImageDto uploadImage(MultipartFile file) {
+        return null;
+    }
 }
