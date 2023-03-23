11a2e30a425c0a1f9bc545b0b43eb5029bb0c254
shgur7236 <shgurtns7236@naver.com>
2023-03-23 17:48:27 +0900

    :sparkles: if문 추가

--- src/main/java/com/ds/ds/domain/group/service/impl/ViewGroupDetailServiceImpl.java
+++ src/main/java/com/ds/ds/domain/group/service/impl/ViewGroupDetailServiceImpl.java
@@ -4,10 +4,11 @@ import com.ds.ds.domain.group.domain.entity.Group;
 import com.ds.ds.domain.group.domain.repository.GroupRepository;
 import com.ds.ds.domain.group.exception.GroupNotFoundException;
 import com.ds.ds.domain.group.presentation.data.dto.DetailGroupDto;
-import com.ds.ds.domain.group.presentation.data.dto.GroupDto;
 import com.ds.ds.domain.group.service.ViewGroupDetailService;
 import com.ds.ds.domain.group.util.GroupConverter;
 import com.ds.ds.domain.member.domain.repository.MemberRepository;
+import com.ds.ds.domain.user.domain.entity.User;
+import com.ds.ds.domain.user.util.UserUtil;
 import com.ds.ds.global.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,13 +20,19 @@ public class ViewGroupDetailServiceImpl implements ViewGroupDetailService {
     private final GroupRepository groupRepository;
     private final GroupConverter groupConverter;
     private final MemberRepository memberRepository;
+    private final UserUtil userUtil;
 
     @Override
     @Transactional(readOnly = true, rollbackFor = Exception.class)
     public DetailGroupDto viewGroupDetail(Long groupIdx) {
+        User user = userUtil.currentUser();
         Group group = groupRepository.findById(groupIdx)
                 .orElseThrow(() -> new GroupNotFoundException(ErrorCode.GROUP_NOT_FOUND));
         Long groupMemberCount = memberRepository.countByGroup(group);
-        return groupConverter.toDto(group, groupMemberCount);
+
+        if(user.equals(group.getUser()) | memberRepository.existsByUserAndGroup(user, group)) {
+            return groupConverter.toDto(group, groupMemberCount, true);
+        } else
+            return groupConverter.toDto(group, groupMemberCount, false);
     }
 }
