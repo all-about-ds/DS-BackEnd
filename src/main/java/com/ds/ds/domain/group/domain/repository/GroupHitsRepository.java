aeea5ca1318a56fadee8779288d156060b25c27c
shgur7236 <shgurtns7236@naver.com>
2023-03-30 17:37:10 +0900

    :sparkles: GroupHitsRepository 생성

---
+++ src/main/java/com/ds/ds/domain/group/domain/repository/GroupHitsRepository.java
@@ -0,0 +1,7 @@
+package com.ds.ds.domain.group.domain.repository;
+
+import com.ds.ds.domain.group.domain.entity.GroupHits;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupHitsRepository extends JpaRepository<GroupHits, Long> {
+}
