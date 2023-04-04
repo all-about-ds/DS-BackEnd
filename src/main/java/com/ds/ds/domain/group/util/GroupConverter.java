fbeca25788e7e5ebf99ba0b284a4920cd79d389a
shgur7236 <shgurtns7236@naver.com>
2023-04-04 17:15:21 +0900

    :recycle: dto 필드값 수정

--- src/main/java/com/ds/ds/domain/group/util/GroupConverter.java
+++ src/main/java/com/ds/ds/domain/group/util/GroupConverter.java
@@ -21,7 +21,7 @@ public interface GroupConverter {
     MemberDto toDto(User user);
     GroupMainDto toDto(Group group, List<MemberDto> list, boolean isHead);
     CreateGroupDto toDto(CreateGroupRequest request);
-    DetailGroupDto toDto(Group group, Long memberCount, Boolean isMember);
+    DetailGroupDto toDto(Boolean isMember);
     GroupDto toDto(Long memberCount, Group group);
     GroupListDto toDto(Pageable pageable, List<GroupDto> dto);
     GroupResponse toResponse(GroupDto dto);
