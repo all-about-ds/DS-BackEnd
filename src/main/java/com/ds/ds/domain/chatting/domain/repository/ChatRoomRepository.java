c1fa8556b854fdbcd5dab1f486631af6b8c2e491
ssyy0122 <z01039118740@gmail.com>
2023-04-05 12:20:36 +0900

    :fire: file remove

--- src/main/java/com/ds/ds/domain/chatting/domain/repository/ChatRoomRepository.java
+++ src/main/java/com/ds/ds/domain/chatting/domain/repository/ChatRoomRepository.java
@@ -1,10 +1,7 @@
 package com.ds.ds.domain.chatting.domain.repository;
 
 import com.ds.ds.domain.chatting.domain.entity.ChatRoom;
-import com.ds.ds.domain.chatting.presentation.data.dto.UserIdDto;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.*;
 
 public interface ChatRoomRepository extends JpaRepository<ChatRoom,String> {
 }
