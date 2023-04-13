d22a604eaaafa1d6dcab6dadbe922cf8becc6ff5
ssyy0122 <z01039118740@gmail.com>
2023-04-13 12:11:38 +0900

    :sparkles: Publish메서드 완성

--- src/main/java/com/ds/ds/domain/chatting/service/RedisMessagePublisher.java
+++ src/main/java/com/ds/ds/domain/chatting/service/RedisMessagePublisher.java
@@ -1,17 +1,15 @@
 package com.ds.ds.domain.chatting.service;
 
-import com.ds.ds.domain.chatting.domain.entity.ChatMessage;
+import com.ds.ds.domain.chatting.presentation.data.dto.ChatMessageDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class RedisMessagePublisher {
     private final RedisTemplate<String, Object> redisTemplate;
-    public void publish(ChatMessage chatMessage) {
+    public void publish(ChatMessageDto chatMessage) {
         redisTemplate.convertAndSend("/sub/chat/room/" + chatMessage.getRoomId(),chatMessage);
-
     }
 }
