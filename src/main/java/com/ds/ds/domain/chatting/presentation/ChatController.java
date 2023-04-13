06dc6ba8f2e9c1fd502efde6383583c166fe4084
ssyy0122 <z01039118740@gmail.com>
2023-04-13 12:18:32 +0900

    :art: contrller 수정

--- src/main/java/com/ds/ds/domain/chatting/presentation/ChatController.java
+++ src/main/java/com/ds/ds/domain/chatting/presentation/ChatController.java
@@ -1,17 +1,17 @@
 package com.ds.ds.domain.chatting.presentation;
 
-import com.ds.ds.domain.chatting.domain.entity.ChatMessage;
+import com.ds.ds.domain.chatting.presentation.data.dto.ChatMessageDto;
 import com.ds.ds.domain.chatting.domain.entity.ChatRoom;
 import com.ds.ds.domain.chatting.domain.repository.ChatRoomRepository;
-import com.ds.ds.domain.chatting.service.RedisMessageSubscriber;
+import com.ds.ds.domain.chatting.presentation.data.request.ChatRequest;
 import com.ds.ds.domain.chatting.service.SendMessageService;
+import com.ds.ds.domain.chatting.util.ChatConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -23,28 +23,18 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class ChatController {
     private final SendMessageService sendMessageService;
-    private final ChatRoomRepository chatRoomRepository;
+
+
     @MessageMapping("/chat.sendMessage")
     @SendTo("/sub/chat/room/{roomId}")
-    public ChatMessage sendMessage(@DestinationVariable String roomId, @Payload ChatMessage chatMessage) {
-        sendMessageService.sendMessage(chatMessage);
-        return chatMessage;
+    public ChatMessageDto sendMessage(@DestinationVariable String roomId, @Payload ChatRequest chatRequest) {
+        return sendMessageService.sendMessage(roomId, chatRequest);
     }
-    @GetMapping("/chat/{roomId}")
-    @ResponseBody
-    public Map<String, Object> chatRoom(@PathVariable String roomId) {
-        Optional<ChatRoom> chatRoom = chatRoomRepository.findById(roomId);
-        if (!chatRoom.isPresent()) {
-            throw new IllegalArgumentException("Invalid roomId: " + roomId);
-        }
-        Map<String, Object> result = new HashMap<>();
-        result.put("roomId", roomId);
-        return result;
-    }
+
     @GetMapping("/chat/history/{roomId}")
     @ResponseBody
-    public ResponseEntity<List<ChatMessage>> getChatHistory(@PathVariable String roomId, @RequestParam("startIndex") Long startIndex, @RequestParam("endIndex") Long endIndex) {
-        List<ChatMessage> chatHistory = sendMessageService.getChatHistory(roomId, startIndex, endIndex);
+    public ResponseEntity<List<ChatMessageDto>> getChatHistory(@PathVariable String roomId, @RequestParam("startIndex") Long startIndex, @RequestParam("endIndex") Long endIndex) {
+        List<ChatMessageDto> chatHistory = sendMessageService.getChatHistory(roomId, startIndex, endIndex);
         return ResponseEntity.ok(chatHistory);
         }
     }
