1f09db1564dbda93f0909eda9fbcf1487acbb00d
shgur7236 <shgurtns7236@naver.com>
2023-04-13 09:13:53 +0900

    :sparkles: group 방장 검사 로직 추가

--- src/main/java/com/ds/ds/domain/member/service/Impl/ExitGroupMemberServiceImpl.java
+++ src/main/java/com/ds/ds/domain/member/service/Impl/ExitGroupMemberServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ds.ds.domain.member.service.Impl;
 
+import com.ds.ds.domain.group.domain.entity.Group;
+import com.ds.ds.domain.group.domain.repository.GroupRepository;
+import com.ds.ds.domain.group.exception.GroupNotFoundException;
 import com.ds.ds.domain.group.exception.NotGroupMemberException;
 import com.ds.ds.domain.member.domain.repository.MemberRepository;
 import com.ds.ds.domain.member.service.ExitGroupMemberService;
@@ -15,14 +18,17 @@ import org.springframework.transaction.annotation.Transactional;
 public class ExitGroupMemberServiceImpl implements ExitGroupMemberService {
     private final UserUtil userUtil;
     private final MemberRepository memberRepository;
+    private final GroupRepository groupRepository;
     @Override
     @Transactional(rollbackFor = Exception.class)
     public void exitGroupMember(Long groupIdx) {
         User user = userUtil.currentUser();
+        Group group = groupRepository.findById(groupIdx)
+                .orElseThrow(() -> new GroupNotFoundException(ErrorCode.GROUP_NOT_FOUND));
 
-        if(!memberRepository.existsByUserIdxAndGroupIdx(user.getIdx(), groupIdx)){
+        if(!memberRepository.existsByUserIdxAndGroupIdx(user.getIdx(), groupIdx) && !group.getUser().equals(user))
             throw new NotGroupMemberException(ErrorCode.NOT_GROUP_MEMBER);
-        }
+
         memberRepository.deleteByUserIdxAndGroupIdx(user.getIdx(), groupIdx);
     }
 }
