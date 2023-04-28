93adbac7ca7d75cd4138efb2f0454ddaded85b3f
jinsiyoun <z01039118740@gmail.com>
2023-04-28 09:43:44 +0900

    :sparkles: 값 교체

--- src/main/java/com/ds/ds/domain/chatting/presentation/data/response/ChatResponse.java
+++ src/main/java/com/ds/ds/domain/chatting/presentation/data/response/ChatResponse.java
@@ -2,6 +2,7 @@ package com.ds.ds.domain.chatting.presentation.data.response;
 
 
 import com.ds.ds.domain.chatting.domain.entity.ChatMessage;
+import com.ds.ds.domain.chatting.presentation.data.type.MessageType;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +16,6 @@ public class ChatResponse {
     private final String sender;
     private final String message;
     private final String roomId;
-    private final ChatMessage.MessageType type;
+    private final MessageType type;
     private final LocalDateTime timestamp;
 }
